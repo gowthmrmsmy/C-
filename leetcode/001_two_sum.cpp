a7a2956109f3d780504f92502dbb7065bafb5dfb
Softybrothers <gowthamramasamy634@gmail.com>
2026-05-27 08:09:59 +0530

    Problem 1

--- leetcode/001_two_sum.cpp
+++ leetcode/001_two_sum.cpp
@@ -20,32 +20,22 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> twoSum(std::vector<int>& nums, int target)
-{
-    // TODO: solve it here
-
-    return {};
-}
-
-int main()
-{
-    // Test case 1
-    std::vector<int> nums1 = {2, 7, 11, 15};
-    int target1 = 9;
-    std::vector<int> result1 = twoSum(nums1, target1);
-    std::cout << "Test 1: [" << result1[0] << ", " << result1[1] << "]\n";
-
-    // Test case 2
-    std::vector<int> nums2 = {3, 2, 4};
-    int target2 = 6;
-    std::vector<int> result2 = twoSum(nums2, target2);
-    std::cout << "Test 2: [" << result2[0] << ", " << result2[1] << "]\n";
-
-    // Test case 3
-    std::vector<int> nums3 = {3, 3};
-    int target3 = 6;
-    std::vector<int> result3 = twoSum(nums3, target3);
-    std::cout << "Test 3: [" << result3[0] << ", " << result3[1] << "]\n";
-
-    return 0;
-}
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            
+             for (int i = 0; i < nums.size(); i++)
+            {
+                for (int j = i + 1; j < nums.size(); j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return {i, j};
+                    }
+                }
+            }
+            return {};
+        }
+        
+    };
+        
